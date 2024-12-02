05b9e62792f06dcefdbee0d4314103e729426f77
shichirouji <shichirouji21@proton.me>
2024-12-02 20:53:24 +0100

    Day 2

--- exe/Main.hs
+++ exe/Main.hs
@@ -5,12 +5,16 @@ import qualified Data.Text.IO as TIO
 import Data.Maybe (fromMaybe)
 import Data.List (sort, group)
 import Data.Time (getCurrentTime, diffUTCTime)
+import Data.Char (isDigit)
+import Data.List (find)
+import qualified Data.Map as Map
+import Data.Map (Map)
 
 main :: IO ()
 main = do
   start <- getCurrentTime
-  day1
-  --day2
+  --day1
+  day2
   --day3
   --day4
   --day5
@@ -38,14 +42,35 @@ day1 = do
 
 day2 :: IO ()
 day2 = do
-  input <- TIO.readFile "data/day2e"
-  putStrLn $ cutePrint "2A" ((solveA) :: Int)
-  putStrLn $ cutePrint "2B" ((solveB) :: Int)
+  input <- TIO.readFile "data/day2"
+  let x = day2Parser (lines (unpack input))
+  putStrLn $ cutePrint "2A" ((solveA x) :: Int)
+  putStrLn $ cutePrint "2B" ((solveB x) :: Int)
   where
-    solveA :: Int
-    solveA = 21 
-    solveB :: Int
-    solveB = 33
+    solveA :: [[Int]] -> Int
+    solveA x = length [f | y <- x, let f = applyFilter y, isValid f y]
+    solveB :: [[Int]] -> Int
+    solveB x = length [y | y <- x, any isValid (y : removeOne y)]
+      where
+        removeOne :: [Int] -> [[Int]]
+        removeOne [] = []
+        removeOne (x:xs) = xs : map (x:) (removeOne xs)
+        isValid :: [Int] -> Bool
+        isValid x = 
+          let f = applyFilter x
+          in not (null f) && (isAscending f || isDescending f) && length x == length f
+    applyFilter :: [Int] -> [Int]
+    applyFilter [] = []
+    applyFilter (x:xs) = go x xs
+      where
+        go p [] = [p]
+        go p (y:ys)
+          | isValidStep p y = p : go y ys
+          | otherwise           = go p ys
+    isValidStep :: Int -> Int -> Bool
+    isValidStep prev z = let diff = z - prev in diff >= 1 && diff <= 3 || diff >= -3 && diff <= -1
+    isValid :: [Int] -> [Int] -> Bool
+    isValid f o = not (null f) && (isAscending f || isDescending f) && length f == length o
 
 -- purity
 
@@ -61,6 +86,24 @@ day1Parser i =
     parse s =
       let [x, y] = map read (words s)
       in  (x, y)
+
+-- ["1 2 3 4 5", "6, 7, 8, 9"] -> [[1, 2, 3, 4, 5], [6, 7, 8, 9]]
+day2Parser :: [String] -> [[Int]]
+day2Parser input = map parse input
+  where
+    parse :: String -> [Int]
+    parse s = map read $ wordsBy (`elem` " ,") s
+    wordsBy :: (Char -> Bool) -> String -> [String]
+    wordsBy p s = case dropWhile p s of
+      "" -> []
+      s' -> w : wordsBy p s''
+        where (w, s'') = break p s'
+
+isAscending :: [Int] -> Bool
+isAscending x = all (> 0) (zipWith (-) (tail x) x)
+
+isDescending :: [Int] -> Bool
+isDescending x = all (< 0) (zipWith (-) (tail x) x)
 
 cutePrint :: (Show a, Num a) => String -> a -> String
 cutePrint s n = "Day " ++ s ++ ": " ++ show (n)
